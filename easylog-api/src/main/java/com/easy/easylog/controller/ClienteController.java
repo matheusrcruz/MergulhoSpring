cc75441696d3d7bcc0608142bc7bfd00061bee82
matheusrcruz <git.dev@outlook.com>
2021-05-20 23:14:43 -0300

    ADD: JPA jakarta persistence specfication

--- easylog-api/src/main/java/com/easy/easylog/controller/ClienteController.java
+++ easylog-api/src/main/java/com/easy/easylog/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.easy.easylog.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +14,14 @@ import com.easy.easylog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext //injeção de dependencia
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente>Listar(){
-	    Cliente cliente1 = new Cliente();
-	    cliente1.setNome("Singed2222");
-		cliente1.setId(1L);
-		cliente1.setTelefone("40028922");
-		cliente1.setEmail("singed@venenoso.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setNome("Ivern11111");
-		cliente2.setId(2L);
-		cliente2.setTelefone("707070707");
-		cliente2.setEmail("Ivern@floresta.com");
-		
-		return Arrays.asList(cliente1, cliente2);  //Retorna Lista cliente1 e cliente2
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
+	   
 	}
 
 }
--- easylog-api/src/main/java/com/easy/easylog/domain/model/Cliente.java
+++ easylog-api/src/main/java/com/easy/easylog/domain/model/Cliente.java
@@ -1,15 +1,32 @@
 package com.easy.easylog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter //Boriplate Lombok get e set(encap)
 @Setter
+@Entity
 public class Cliente {
-								
+	
+	@EqualsAndHashCode.Include //Equals and hashCode
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
+	
 	private String email;
+	
+	@Column(name ="fone")
 	private String telefone;
 }
 
